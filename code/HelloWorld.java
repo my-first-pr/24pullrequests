016313c31a6d0732edf2a4d6f60d35bbbc2721a7
Zee <44004976+F-Zee@users.noreply.github.com>
2018-10-10 21:50:02 +0200

    Create HelloWorld.java
    A simple java code to print "Hello, World!" on the console.

---
+++ code/HelloWorld.java
@@ -0,0 +1,7 @@
+//a simple java code to print "Hello, World!" on the console.
+public class HelloWorld{
+public static void main (String[] args){
+System.out.println("Hello, World!");
+//Thanks for your time!
+}
+}
